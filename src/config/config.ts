7645f04355ba4d8ec4768a0c5d52acdc1f618bfa
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2022-03-07 23:38:45 +0530

    init: supabase

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,7 @@
-export {};
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://cgxnuaruwknqjgysuugt.supabase.co";
+
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNneG51YXJ1d2tucWpneXN1dWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDYzMTcxNDQsImV4cCI6MTk2MTg5MzE0NH0.WCaV5nVlAUdqnlyBZpkxDLdjEst33o02sB3m2UVNcRs";
+export const supabase = createClient(supabaseUrl, supabaseKey);
